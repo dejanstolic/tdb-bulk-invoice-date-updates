6692b5b5deb175cb2f811370164cb07a1f586682
Djordje Gavrilovic <gavrilovicmdj@gmail.com>
2021-09-15 15:33:23 +0200

    servlet init

---
+++ src/main/java/com/brandmaker/cs/skyhigh/tdb/servlets/CrossChargesServlet.java
@@ -0,0 +1,24 @@
+package com.brandmaker.cs.skyhigh.tdb.servlets;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class CrossChargesServlet extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+    private static final Log LOG = LogFactory.getLog(CrossChargesServlet.class);
+
+    public CrossChargesServlet() {super();}
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) {
+
+    }
+
+
+}
